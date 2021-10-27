48664c3868de09438d7350f933d48238ee80007d
Dennis Kim <dkim334@my.bcit.ca>
2021-10-26 20:51:58 -0700

    login done

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,13 +1,11 @@
 // Your web app's Firebase configuration
-var  firebaseConfig = {
-    apiKey: ,
-    authDomain: ",
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
-  };
-  
+var firebaseConfig = {
+  apiKey: "AIzaSyB1JtlQYYUFE7Du0uVAi9SxC9OEaPl0pLc",
+  authDomain: "kim-dennis-demo08.firebaseapp.com",
+  projectId: "kim-dennis-demo08",
+  storageBucket: "kim-dennis-demo08.appspot.com",
+  messagingSenderId: "35969621937",
+  appId: "1:35969621937:web:4201ed319384a5687f6790",
+};
 
-  const app = firebase.initializeApp(firebaseConfig);
-  
+const app = firebase.initializeApp(firebaseConfig);
